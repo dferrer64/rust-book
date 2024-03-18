ce3038ea9eacaf349021b88da8374664a2ca2525
Diego Ferrer <ferrerd@ornl.gov>
2024-03-18 09:44:36 +0000

    nth_fibonacci: Add comments

--- nth_fibonacci/src/main.rs
+++ nth_fibonacci/src/main.rs
@@ -1,28 +1,36 @@
-use std::io;
-use std::io::Write;
+// Author: Diego Ferrer
+// Task: 
+
+use std::io; // needed for reading values from stdin
+use std::io::Write; // needed to flush output buffer
 
 fn main() {
+    // initialize a mutable string
     let mut n = String::new();
 
+    // first prompt
     print!("Choose a value for n: ");
+
+    // read response, store it in n, and flush the buffer
     std::io::stdout().flush().unwrap();
     io::stdin().read_line(&mut n).expect("Failed to read n.");
 
+    // convert string response to u128 type
     let mut n: u128 = n.trim().parse().expect("Non-integer value input!");
     let nth: u128 = n;
 
-    //
+    // handle n=0 case
     if n < 1 {
-        println!("The Fibonacci Series is indexed on 1!");
+        println!("The Fibonacci Series begins where fn (n=1) is 1!");
         return;
     }
 
-    //
+    // initialize sequence
     let mut x0: u128;
     let mut x1: u128 = 1;
     let mut x2: u128 = 1;
 
-    //
+    // calculate nth fibonacci
     while n > 1 {
         x0 = x1;
         x1 = x2;
@@ -31,5 +39,6 @@ fn main() {
         n = n - 1;
     }
 
+    // print result
     println!("(n={nth}) nth element: {x1}")
 }
